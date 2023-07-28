f8a62cf18b41718d17e109815558b21e8a148d91
Vikastgn <vikamarygina@yandex.ru>
2023-07-28 17:45:17 +0300

    initial commit

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,8 +6,8 @@
 // 
 let number1 = Math.floor(Math.random() * 100);
 if (number1 % 2===0){
-console.log (number1+ ':четное число')
+console.log (number1+ 'четное число')
 }
 else {
-    console.log (number1+ ':нечетное число')
+    console.log (number1+ 'нечетное число')
 }
