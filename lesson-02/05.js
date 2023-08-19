704b365a046040338fbb9b1e61ce915c6376eeaa
Vikastgn <vikamarygina@yandex.ru>
2023-08-19 17:24:02 +0300

    initial commit

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -3,6 +3,14 @@ let passport = {
     surname: "Petrov",
 };
 let passport2 = {...passport};
-passport.name ='Ivan'
+passport2.name ='Ivan'
 console.log (passport);
 console.log (passport2)
+
+
+// let user = { name: 'Igor' };
+// //let user2 = user;
+// let user2 = { ...user };
+// user.name = 'Nastya';
+// console.log(user);
+// console.log(user2);
