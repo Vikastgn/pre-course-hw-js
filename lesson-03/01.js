b482a17507787b7bf8db5bf1058f0b616601bdaa
Vikastgn <vikamarygina@yandex.ru>
2023-08-27 08:24:09 +0300

    initial commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return flagColor=colors.filter(color => color ==="черный" || color ==="желтый" || color ==="красный").join('-')
+   
+}
+
+console.log(createColorString());
